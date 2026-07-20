200ddd498923627ddb30c9f0e8b1a64f430edd35
supermx1 <emeka_toby@yahoo.com>
2026-07-21 00:20:03 +0100

    chore: update Microsoft Clarity project ID in SEO component

--- src/lib/components/SEO.svelte
+++ src/lib/components/SEO.svelte
@@ -52,18 +52,21 @@
     <!-- eslint-disable-next-line svelte/no-at-html-tags -->
     {@html schemaMarkup}
   {/if}
-  <script type="text/javascript">
-    (function (c, l, a, r, i, t, y) {
-      c[a] =
-        c[a] ||
-        function () {
-          (c[a].q = c[a].q || []).push(arguments);
-        };
-      t = l.createElement(r);
-      t.async = 1;
-      t.src = "https://www.clarity.ms/tag/" + i;
-      y = l.getElementsByTagName(r)[0];
-      y.parentNode.insertBefore(t, y);
-    })(window, document, "clarity", "script", "wjhwo29ppr");
-  </script>
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "xpmu4kzenb");
+</script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-VL6FMWHB46"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-VL6FMWHB46');
+</script>
+
 </svelte:head>
